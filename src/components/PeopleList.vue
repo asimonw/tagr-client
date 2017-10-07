25949ae71e2d9904e11aea3014af104aa64eace7
asimonw <awijns@gmail.com>
2017-10-07 21:59:29 +0200

    Initial commit - generated by vue-cli

---
+++ src/components/PeopleList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <ul>
+      <li v-for="person in people">{{ person.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PeopleList',
+  data() {
+    return {
+      title: 'People',
+      people: [
+        { name: 'Alex' },
+        { name: 'Fanny' },
+      ],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
