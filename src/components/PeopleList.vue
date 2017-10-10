b7a74889531bbf1d0a04fa4b4d7fe0aac1b9c4e9
asimonw <awijns@gmail.com>
2017-10-11 00:13:52 +0200

    Set up feathers and test authentication

--- src/components/PeopleList.vue
+++ src/components/PeopleList.vue
@@ -4,10 +4,25 @@
     <ul>
       <li v-for="person in people">{{ person.name }}</li>
     </ul>
+    <a href="http://localhost:3000/auth/github" class="button">Login With GitHub</a>
+    <button @click="findUsers">Request</button>
   </div>
 </template>
 
 <script>
+import feathers from 'feathers/client';
+import rest from 'feathers-rest/client';
+import hooks from 'feathers-hooks';
+// import errors from 'feathers-errors';
+import auth from 'feathers-authentication-client';
+
+const client = feathers()
+  .configure(rest('http://localhost:3000').fetch(fetch))
+  .configure(hooks())
+  .configure(auth());
+
+const userService = client.service('/users');
+
 export default {
   name: 'PeopleList',
   data() {
@@ -18,6 +33,15 @@ export default {
         { name: 'Fanny' },
       ],
     };
+  },
+  methods: {
+    findUsers() {
+      userService.find()
+        // eslint-disable-next-line
+        .then(result => console.log(result))
+        // eslint-disable-next-line
+        .catch(error => console.log(error));
+    },
   },
 };
 </script>
